c8ef59876e4117f7548d7e25bf2008094c8fa7df
Lucas-web-byte <121920714+Lucas-web-byte@users.noreply.github.com>
2024-10-16 22:22:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,13 @@
 <body>
 	<header class="header">
 		<nav class="menu">
+			<img src"cinepanda.jpg" alt="Logo Cinepanda" class="logo">
+		<div class="menu-right">
 			  <a href="" class="menu-item">Home</a>
 			  <a href="" class="menu-item">Em cartaz</a>
 			  <a href="" class="menu-item">Contato</a>
 			  <a href="" class="menu-item">Glossário</a>
+		</div>
 		</nav>
 		<div class="header-title">Cinepanda</div>
 	</header>
